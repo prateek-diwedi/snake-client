b8b1737a4a315f68319484b13beddc81a4c39a83
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-05 01:41:19 +0000

    updated game

--- play.js
+++ play.js
@@ -1,18 +1,9 @@
 // play.js
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
- const handleUserInput = function(data){
-  if (key === '\u0003') {
-    console.log('Thanks for using me, ciao!')
-    process.exit();
-  }
-  return stdin;
- }
-}
-
 const  connect  = require('./client');
+const  setupInput  = require('./input');
+
+
 console.log('Connecting ...');
 connect();
+
+setupInput();
