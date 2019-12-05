ee289b58be97a1996efe056979261bb52c13a87d
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-05 00:05:44 +0000

    updated code

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 // play.js
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+ const handleUserInput = function(data){
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!')
+    process.exit();
+  }
+  return stdin;
+ }
+}
+
 const  connect  = require('./client');
 console.log('Connecting ...');
 connect();
